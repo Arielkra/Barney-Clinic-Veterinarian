3fdae2e322cb84dd915b3f3c75d9b64dcad48e90
Ariel Kravizki <88767802+Arielkra@users.noreply.github.com>
2024-01-17 11:16:12 +0200

    Update mailSender.cs

--- Management of a veterinary clinic/Access/mailSender.cs
+++ Management of a veterinary clinic/Access/mailSender.cs
@@ -44,7 +44,7 @@ using System.Threading.Tasks;
 
     public static bool SendMail2(List<string> to, string subject, string message)
     {
-        try
+       try
         {
             SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
 
@@ -54,13 +54,11 @@ using System.Threading.Tasks;
                 mail.To.Add(user);
 
             }
-            
-            mail.Subject = subject;
-
+        
             mail.Body = "<b>" + message + "<h3><b><br><br>בתודה , המרפאה הוטרינרית בארני,באר שבע. טל : 08-6668888</h3></b></br>" + "\n" + @"<img src=""https://lh3.googleusercontent.com/pw/ACtC-3fuOb_Rvpf2Frpiw3ZTQeef2JxA7yJGJr_ulLuc367R8B_0vu5kDjhFCiJ1BAXWT5eAX7lS2rpgM56z8Cr18xnbTWSK7e5SmlkHQC761jqnWtb6UOKKmBuxqBOf_FmocdWHJJfdeTuvGcmAZurtTn9DpA=s72-no?authuser=0""/>";
             mail.IsBodyHtml = true;
             SmtpServer.Port = 587;
-            SmtpServer.Credentials = new System.Net.NetworkCredential("guyariel75@gmail.com", "02092020ga");
+            SmtpServer.Credentials = new System.Net.NetworkCredential("guyariel75@gmail.com", "utmn awtv pfql cbsp");
             SmtpServer.EnableSsl = true;
 
             SmtpServer.Send(mail);
