29c112ecbfc9cf3e42e9d20d879a1a9225fee364
Ariel Kravizki <88767802+Arielkra@users.noreply.github.com>
2024-01-28 18:47:05 +0200

    Update mailSender.cs

--- Management of a veterinary clinic/Access/mailSender.cs
+++ Management of a veterinary clinic/Access/mailSender.cs
@@ -29,7 +29,7 @@ using System.Threading.Tasks;
             mail.Body = "<b>" + message + "<h3><b><br><br>בתודה , המרפאה הוטרינרית בארני,באר שבע. טל : 08-6668888</h3></b></br>" + "\n" + @"<img src=""https://lh3.googleusercontent.com/pw/ACtC-3fuOb_Rvpf2Frpiw3ZTQeef2JxA7yJGJr_ulLuc367R8B_0vu5kDjhFCiJ1BAXWT5eAX7lS2rpgM56z8Cr18xnbTWSK7e5SmlkHQC761jqnWtb6UOKKmBuxqBOf_FmocdWHJJfdeTuvGcmAZurtTn9DpA=s72-no?authuser=0""/>";
             mail.IsBodyHtml = true;
             SmtpServer.Port = 587;
-            SmtpServer.Credentials = new System.Net.NetworkCredential("guyariel75@gmail.com", "utmn awtv pfql cbsp");
+            SmtpServer.Credentials = new System.Net.NetworkCredential("guyariel75@gmail.com", "dcpr jxcm dxva xier");
             SmtpServer.EnableSsl = true;
 
             SmtpServer.Send(mail);
@@ -58,7 +58,7 @@ using System.Threading.Tasks;
             mail.Body = "<b>" + message + "<h3><b><br><br>בתודה , המרפאה הוטרינרית בארני,באר שבע. טל : 08-6668888</h3></b></br>" + "\n" + @"<img src=""https://lh3.googleusercontent.com/pw/ACtC-3fuOb_Rvpf2Frpiw3ZTQeef2JxA7yJGJr_ulLuc367R8B_0vu5kDjhFCiJ1BAXWT5eAX7lS2rpgM56z8Cr18xnbTWSK7e5SmlkHQC761jqnWtb6UOKKmBuxqBOf_FmocdWHJJfdeTuvGcmAZurtTn9DpA=s72-no?authuser=0""/>";
             mail.IsBodyHtml = true;
             SmtpServer.Port = 587;
-            SmtpServer.Credentials = new System.Net.NetworkCredential("guyariel75@gmail.com", "utmn awtv pfql cbsp");
+            SmtpServer.Credentials = new System.Net.NetworkCredential("guyariel75@gmail.com", "dcpr jxcm dxva xier");
             SmtpServer.EnableSsl = true;
 
             SmtpServer.Send(mail);
